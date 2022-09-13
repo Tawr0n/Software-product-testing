d072d4e273750e5c30bafbf50504f76eadd4a072
Tawr0n <yaroslav.kit.it@gmail.com>
2022-09-13 17:11:44 +0300

    Lab1

---
+++ Lab1/AnalizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b6558e6-7c4c-48b4-b6ce-8aaec65aa2f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab1/AnalizerClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+using System;
+
+namespace AnalaizerClassLibraryTests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "testData.xml",
+            "Test",
+            DataAccessMethod.Sequential)]
+
+        [TestMethod()]
+        public void RunEstimateTest()
+        {
+            // Arrange
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+            AnalaizerClass.expression = Convert.ToString(TestContext.DataRow["initialized"]);
+
+            // Actual
+            string actual = AnalaizerClass.RunEstimate();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+    }
+}
